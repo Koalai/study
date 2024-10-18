8bc58ed0791569c14b84a1c7c506c61c74ea7aee
Dang Khoa <dangkhoanguyen0812@gmail.com>
2024-10-18 14:24:57 +0700

    p3-ex3.9 => ex3.12

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,27 +7,33 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://dangkhoanguyen0812:${password}@cluster0.rvl5f.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://dangkhoanguyen0812:${password}@cluster0.rvl5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model('Note', noteSchema);
+const Person = mongoose.model('Person', personSchema);
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-});
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((p) => {
+      console.log(`${p.name} ${p.number}`);
+    });
+    mongoose.connection.close()
   });
-  mongoose.connection.close();
-});
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+  });
+  mongoose.connection.close()
+}
