59950b656c4fa77f5349cd393dbb4961719a47f2
Dang Khoa <dangkhoanguyen0812@gmail.com>
2024-10-18 11:16:31 +0700

    install cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 
 let people = [
@@ -25,6 +26,7 @@ let people = [
   },
 ];
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan('tiny'));
 
